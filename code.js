de6e1f3ffdb23c87b83ea6b435d96b27b8e599b7
Lora Zubic <lorazubic@Loras-MacBook-Pro.local>
2023-11-14 15:30:04 +0100

    player moves

--- code.js
+++ code.js
@@ -9,20 +9,68 @@ document.addEventListener("DOMContentLoaded", () => {
         constructor() {
             this.x = mainCanvas.width/2
             this.y = mainCanvas.height/2
-            this.width = 20
-            this.height = 20
+            this.width = 30
+            this.height = 30
             this.color = "red"
         }
 
         draw() {
             mainContext.fillStyle = this.color
             mainContext.fillRect(this.x, this.y, this.width, this.height)
-            print('huhue')
+        }
+
+        clear() {
+            mainContext.clearRect(this.x, this.y, this.width, this.height);
         }
 
     }
 
+    class Enemy {
+        constructor() {
+            this.x = Math.random() * window.innerWidth
+            this.y = -20; // Početak izvan ekrana
+            this.width = 20;
+            this.height = 20;
+            this.color = 'grey';
+            this.velocity = {
+                x: (Math.random() - 0.5) * 4,
+                y: Math.random() * 3 + 1
+            };
+        }
+
+        draw() {
+            mainContext.fillStyle = this.color;
+            mainContext.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
     const player = new Player();
     player.draw()
+
+    window.addEventListener('keydown', (e) => {
+        player.clear()
+        switch (e.key) {
+            case 'ArrowUp':
+                if(player.y > 0) {
+                    player.y -= 10
+                }
+                break;
+            case 'ArrowDown':
+                if(player.y < mainCanvas.height - player.height) {
+                    player.y += 10
+                }
+                break;
+            case 'ArrowLeft':
+                if(player.x > 0) {
+                    player.x -= 10
+                }
+                break;
+            case 'ArrowRight':
+                if(player.x < mainCanvas.width - player.width) {
+                    player.x += 10
+                }
+                break;
+        }
+        player.draw()
+    })
 })
 
