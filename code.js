0b55f9729782f9fd9ff3534319f4fb8bbdd4481a
Lora Zubic <lorazubic@Loras-MacBook-Pro.local>
2023-11-14 14:37:27 +0100

    player visible

--- code.js
+++ code.js
@@ -1,9 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
     var mainCanvas = document.getElementById("game");
-    var mainContex = mainCanvas.getContext("2d");
+    var mainContext = mainCanvas.getContext("2d");
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    mainCanvas.width = window.innerWidth;
+    mainCanvas.height = window.innerHeight;
 
+    class Player {
+        constructor() {
+            this.x = mainCanvas.width/2
+            this.y = mainCanvas.height/2
+            this.width = 20
+            this.height = 20
+            this.color = "red"
+        }
+
+        draw() {
+            mainContext.fillStyle = this.color
+            mainContext.fillRect(this.x, this.y, this.width, this.height)
+            print('huhue')
+        }
+
+    }
+
+    const player = new Player();
+    player.draw()
 })
 
