40c0922b92b1ac5e1278c49b3aa349548ad08079
Lora Zubic <lorazubic@Loras-MacBook-Pro.local>
2023-11-14 14:13:21 +0100

    base canvas done

---
+++ code.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var mainCanvas = document.getElementById("game");
+    var mainContex = mainCanvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+})
+
