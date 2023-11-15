5cbb35b5b3e522568ea125b387407079602b5fe3
Lora Zubic <lorazubic@Loras-MacBook-Pro.local>
2023-11-15 10:24:00 +0100

    colision recognicion added

--- code.js
+++ code.js
@@ -7,8 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     class Player {
         constructor() {
-            this.x = mainCanvas.width/2
-            this.y = mainCanvas.height/2
+            this.x = mainCanvas.width / 2
+            this.y = mainCanvas.height / 2
             this.width = 30
             this.height = 30
             this.color = "red"
@@ -27,44 +27,46 @@ document.addEventListener("DOMContentLoaded", () => {
 
     class Enemy {
         constructor(direction) {
-            if(direction === "top") {
+            if (direction === "top") {
                 this.x = Math.round(Math.random() * window.innerWidth)
                 this.y = -20; // Početak izvan ekrana
-            } else if(direction === "left") {
+            } else if (direction === "left") {
                 this.x = -20
                 this.y = Math.round(Math.random() * window.innerHeight)
-            } else if(direction === "right") {
+            } else if (direction === "right") {
                 this.x = window.innerWidth + 20
                 this.y = Math.round(Math.random() * window.innerHeight)
             } else {
                 this.x = Math.round(Math.random() * window.innerWidth)
                 this.y = window.innerHeight + 20
             }
-            this.width = 30;
-            this.height = 30;
-            this.color = 'grey';
+            this.width = 30
+            this.height = 30
+            this.color = 'grey'
         }
 
         draw() {
             mainContext.fillStyle = this.color;
-            mainContext.fillRect(this.x, this.y, this.width, this.height);
+            mainContext.fillRect(this.x, this.y, this.width, this.height)
         }
 
         clear() {
-            mainContext.clearRect(this.x, this.y, this.width, this.height);
+            mainContext.clearRect(this.x, this.y, this.width, this.height)
         }
     }
-    const player = new Player();
+    const player = new Player()
     player.draw()
 
     const enemies = []
-    setInterval(generateEnemies, 8000);
+    setInterval(generateEnemies, 4000)
+
+    setInterval(checkForOverlapps, 100)
 
     function generateEnemies() {
-        const enemyTop = new Enemy('top');
-        const enemyLeft = new Enemy('left');
-        const enemyRight = new Enemy('right');
-        const enemyDown = new Enemy('down');
+        const enemyTop = new Enemy('top')
+        const enemyLeft = new Enemy('left')
+        const enemyRight = new Enemy('right')
+        const enemyDown = new Enemy('down')
         enemies.push(enemyTop)
         enemies.push(enemyLeft)
         enemies.push(enemyRight)
@@ -74,11 +76,14 @@ document.addEventListener("DOMContentLoaded", () => {
         var leftInterval = setInterval(newEnemyFromLeft, 100);
         var rightInterval = setInterval(newEnemyFromRight, 100);
         var downInterval = setInterval(newEnemyFromDown, 100);
-     
-        function newEnemyFromTop() { 
-            if(enemyTop.y > window.innerHeight) {
+
+        function newEnemyFromTop() {
+            if (enemyTop.y > window.innerHeight) {
                 clearInterval(topInterval)
-                enemies.pop(enemyTop)
+                let index = enemies.indexOf(enemyTop);
+                if (index > -1) {
+                    enemies.splice(index, 1); 
+                }
                 enemyTop.clear()
             } else {
                 enemyTop.clear()
@@ -88,9 +93,12 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         function newEnemyFromLeft() {
-            if(enemyLeft.x > window.innerWidth) {
+            if (enemyLeft.x > window.innerWidth) {
                 clearInterval(leftInterval)
-                enemies.pop(enemyLeft)
+                let index = enemies.indexOf(enemyLeft);
+                if (index > -1) {
+                    enemies.splice(index, 1); 
+                }
                 enemyLeft.clear()
             } else {
                 enemyLeft.clear()
@@ -99,10 +107,13 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
 
-        function newEnemyFromRight() { 
-            if(enemyRight.x < 0) {
+        function newEnemyFromRight() {
+            if (enemyRight.x < 0) {
                 clearInterval(rightInterval)
-                enemies.pop(enemyRight)
+                let index = enemies.indexOf(enemyRight);
+                if (index > -1) {
+                    enemies.splice(index, 1); 
+                }
                 enemyRight.clear()
             } else {
                 enemyRight.clear()
@@ -112,9 +123,12 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         function newEnemyFromDown() {
-            if(enemyDown.y < 0) {
+            if (enemyDown.y < 0) {
                 clearInterval(downInterval)
-                enemies.pop(enemyDown)
+                let index = enemies.indexOf(enemyDown);
+                if (index > -1) {
+                    enemies.splice(index, 1); 
+                }
                 enemyDown.clear()
             } else {
                 enemyDown.clear()
@@ -123,27 +137,55 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
     }
+    
+    function areRectanglesOverlapping(rect1, rect2) {
+        let minAx = rect1.x;
+        let minAy = rect1.y;
+        let maxAx = rect1.x + rect1.width;
+        let maxAy = rect1.y + rect1.height;
+    
+        let minBx = rect2.x;
+        let minBy = rect2.y;
+        let maxBx = rect2.x + rect2.width;
+        let maxBy = rect2.y + rect2.height;
+    
+        let aLeftOfB = maxAx < minBx;
+        let aRightOfB = minAx > maxBx;
+        let aAboveB = minAy > maxBy;
+        let aBelowB = maxAy < minBy;
+    
+        return !(aLeftOfB || aRightOfB || aAboveB || aBelowB);
+    }
+    
+
+    function checkForOverlapps() {
+        for (let enemy of enemies) {
+            if (areRectanglesOverlapping(enemy, player)) {
+                console.log("Colision detected")
+            }
+        }
+    }
 
     window.addEventListener('keydown', (e) => {
         player.clear()
         switch (e.key) {
             case 'ArrowUp':
-                if(player.y > 0) {
+                if (player.y > 0) {
                     player.y -= 10
                 }
                 break;
             case 'ArrowDown':
-                if(player.y < mainCanvas.height - player.height) {
+                if (player.y < mainCanvas.height - player.height) {
                     player.y += 10
                 }
                 break;
             case 'ArrowLeft':
-                if(player.x > 0) {
+                if (player.x > 0) {
                     player.x -= 10
                 }
                 break;
             case 'ArrowRight':
-                if(player.x < mainCanvas.width - player.width) {
+                if (player.x < mainCanvas.width - player.width) {
                     player.x += 10
                 }
                 break;
